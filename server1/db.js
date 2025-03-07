6e14957b55ad0b4a7560a4c20738bfa1ea7fe9b8
Gokulanath-kk <gokulanath.kkg@gmail.com>
2025-03-07 12:40:18 +0530

    first commit

--- server1/db.js
+++ server1/db.js
@@ -2,11 +2,11 @@ const sql = require('mysql2');
 
 
 const con = sql.createPool({
-    host: "node-database.cle6q2c8ewg3.eu-north-1.rds.amazonaws.com",
+    host: "node-database-xplore.cle6q2c8ewg3.eu-north-1.rds.amazonaws.com",
     port: "3306",
     user: "root",
     password: "root",
     database: "quiz_master"
   });
 
-  module.exports = con;
+  module.exports = con; 
