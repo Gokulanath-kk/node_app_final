817f7427e97a991c6e1d813c20c08092d0cb0f89
Gokulanath-kk <gokulanath.kkg@gmail.com>
2025-03-07 15:37:22 +0530

    Update db.js

--- server1/db.js
+++ server1/db.js
@@ -2,10 +2,10 @@ const sql = require('mysql2');
 
 
 const con = sql.createPool({
-    host: "node-database-xplore.cle6q2c8ewg3.eu-north-1.rds.amazonaws.com",
+    host: "nodedb.cle6q2c8ewg3.eu-north-1.rds.amazonaws.com",
     port: "3306",
     user: "root",
-    password: "root",
+    password: "Admin@123",
     database: "quiz_master"
   });
 
